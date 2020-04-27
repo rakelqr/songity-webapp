d79b6a6bbbd742e17abc23b13bd840b376a452c6
RakelQR <raquelqrodriguez@gmail.com>
2020-04-27 17:34:53 +0200

    first swagger doc

---
+++ backend/docs/mainDoc.js
@@ -0,0 +1,18 @@
+export const swaggerDocument = {
+    openapi: '3.0.1',
+    info: {
+        version: '1.0.0',
+        title: 'Backend',
+        description: "Backend side of Sounditi's project",
+        termsOfService: '',
+        contact: {
+            name: 'Raquel Q. Rodríguez',
+            email: 'raquelqrodriguez@gmail.com',
+            url: 'https://github.com/rakelqr'
+        },
+        license: {
+            name: 'Apache 2.0',
+            url: 'https://www.apache.org/licenses/LICENSE-2.0.html'
+        }
+    }
+}
