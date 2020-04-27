645164f68a526ae18074839e9e5300c30642d17c
RakelQR <raquelqrodriguez@gmail.com>
2020-04-27 23:28:35 +0200

    config updated

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,12 +1,13 @@
 require('dotenv').config();
 import { MongoClient } from 'mongodb';
 
-const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017';
+const connectionString = process.env.MONGODB_URI;
+const database = process.env.DATABASE
 let db;
 
 const init = () => MongoClient.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(client => {
-    db = client.db('Sounditi');
+    db = client.db(database);
     return db;
   });
 
